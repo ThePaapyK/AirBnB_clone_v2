a8bcd9e61942d6d78edf553649e5092a3e4e70e1
Paa Kojo Annan <paaqwadwo419@gmail.com>
2022-12-30 01:00:03 +0000

    o

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -2,7 +2,7 @@
 """full deployment to web servers"""
 do_pack = __import__('1-pack_web_static').do_pack
 do_deploy = __import__('2-do_deploy_web_static').do_deploy
-
+env.hosts = ['3.94.185.28', '35.153.17.98']
 
 def deploy():
     """calls two functions to fully deploy
