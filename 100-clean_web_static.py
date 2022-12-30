f685df1b0c082e6b2a98d9593cfcbc352eb1c0ac
Paa Kojo Annan <paaqwadwo419@gmail.com>
2022-12-30 13:47:18 +0000

    Kool

---
+++ 100-clean_web_static.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+"""full deployment to web servers"""
+import os.path
+from fabric.api import *
+from datetime import datetime
+env.hosts = ['3.94.185.28', '35.153.17.98']
+
+
+def do_pack():
+    """
+    making an archive on web_static folder
+    """
+    try:
+        time = datetime.now()
+        archive = 'web_static_' + time.strftime("%Y%m%d%H%M%S") + '.' + 'tgz'
+        local('mkdir -p versions')
+        local('tar -cvzf versions/{} web_static'.format(archive))
+        return ("versions/{}".format(archive))
+    except Exception:
+        return None
+
+
+def do_deploy(archive_path):
+    """deploy the tgz item to my web servers"""
+    if (os.path.isfile(archive_path) is False):
+        return False
+
+    try:
+        archive = archive_path.split('/')[-1]
+        path = '/data/web_static/releases/' + archive.strip('.tgz')
+        symp = '/data/web_static/current'
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}/'.format(path))
+        run('tar -xzf /tmp/{} -C {}'.format(archive, path))
+        run('rm /tmp/{}'.format(archive))
+        run('mv {}/web_static/* {}'.format(path, path))
+        run('rm -rf {}/web_static'.format(path))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {} {}'.format(path, symp))
+        return True
+    except Exception:
+        return False
+
+
+def deploy():
+    """calls two functions to fully deploy
+    the web_static folder
+    """
+    try:
+        archive_name = do_pack()
+        item = do_deploy(archive_name)
+        return item
+    except Exception:
+        return False
+
+def do_clean(number=0):
+    """deletes outdated archives"""
+    if number == 0 or number == 1:
+        with cd.local('./versions/'):
+            local("ls -lv | rev | cut -f 1 | rev | \
+            head -n +1 | xargs -d '\n' rm -rf")
+        with cd('/data/web_static/releases/'):
+            run("sudo ls -lv | rev | cut -f 1 | \
+            rev | head -n +1 | xargs -d '\n' rm -rf")
+        else:
+
+            with cd.local('./versions/'):
+                local("ls -lv | rev | cut -f 1 | rev | \
+                head -n +{} | xargs -d '\n' rm -rf".format(number))
+            with cd('/data/web_static/releases/'):
+                run("sudo ls -lv | rev | cut -f 1 | \
+                rev | head -n +{} | xargs -d '\n' rm -rf".format(number))
