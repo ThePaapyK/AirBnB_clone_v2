c311f2e9dc7acddc5835efa8968cc6f09882a198
Paa Kojo Annan <paaqwadwo419@gmail.com>
2023-01-15 20:43:43 +0000

    New one

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Start a basic Flask web application"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """basic Flask web application"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """displays 'HBNB'"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def text():
+    """display “C ” followed by the \
+    value of the text variable \
+    (replace underscore _ symbols with a space )
+    """
+    return 'C %s'.replace('_', ' ') % escape(text)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
