910e9d7449499b33dfde08957c3f97c580862905
Paa Kojo Annan <paaqwadwo419@gmail.com>
2023-01-15 22:07:03 +0000

    number

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+""" Start a basic Flask web application"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """basic Flask web application"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """displays 'HBNB'"""
+    return 'HBNB'
+
+
+@app.route('/c/<string:text>', strict_slashes=False)
+def text(text=None):
+    """display “C ” followed by the \
+    value of the text variable \
+    (replace underscore _ symbols with a space )
+    """
+    return 'C {}'.format(text.replace("_", " "))
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<string:text>',  strict_slashes=False)
+def pyth(text="is cool"):
+    """ display “Python ”, followed by the value of the text"""
+    return 'Python {}'.format(text.replace("_", " "))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def num(n=None):
+    """display “n is a number” only if n is an integer"""
+    return '{} is a number'.format(n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
